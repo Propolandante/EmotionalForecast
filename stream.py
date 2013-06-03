9a83092c8aa5ac4afa4ef0209d5a0d3e9c3884d3
Propolandante <propolandante@gmail.com>
2013-06-02 19:08:46 -0700

    stream.py now filters out retweets and writes desired tweet info from stream to text file

--- stream.py
+++ stream.py
@@ -1,16 +1,19 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import json
+import datetime
+
 
 # Go to http://dev.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key="3CILoRRD7cW9zynmS5hQ"
-consumer_secret="tvuFMAw5YcEsggk6xeSNGAI3qey8YYh8q01B28uupA"
+consumer_key=""
+consumer_secret="tvuFMAw5YcEsggk6xeSNGAIqey8YYh8q01B28uupA"
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token="17454258-4YPtYrWSSvaHcxGn8o4c3Pq6rhdNSxH8LYIlFfq90"
-access_token_secret="y2XvTslVxYNqqQaB5RUOurBO9Pw7dJd5w7rQk5s"
+access_token=""
+access_token_secret=""
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
@@ -18,11 +21,34 @@ class StdOutListener(StreamListener):
 
     """
     def on_data(self, data):
-        print data
+
+        if 'retweeted_status' in data:
+            return True
+
+        
+        
+
+        f = open('060213_2.txt','a+')
+        
+        text = json.loads(data)['text']
+        time = json.loads(data)['created_at']
+        tweet = json.loads(data)['id_str']
+        
+        f.write(tweet.encode('utf-8') + "\n")
+        f.write(time.encode('utf-8)') + "\n")
+        f.write(text.encode('utf-8)') + "\n")
+        
+        f.write("\n")
+        
+        f.close()
+        print "working as of "
+        print datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
         return True
+
 
     def on_error(self, status):
         print status
+        return True
 
 if __name__ == '__main__':
     l = StdOutListener()
